558ddc477b9207eaeb4c5069fd43af21b6cce71e
Andrey <andreieminov@gmail.com>
2020-05-07 12:15:32 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import Slider from 'infinite-react-carousel';
+import './App.css';
+import SliderComponent from "./components/common/Slider/SliderComponent";
+
+function App() {
+
+  return (
+      <div>
+          <SliderComponent/>
+      </div>
+  );
+}
+
+export default App;
+
